79460a34ad887fa2ef7df75227c201a0fec0ee5b
Narumichi Kubo <narumichi7373@gmail.com>
2023-07-09 18:10:28 +0900

    update project setting

--- Demo/Demo/ContentView.swift
+++ Demo/Demo/ContentView.swift
@@ -6,14 +6,62 @@
 //
 
 import SwiftUI
+import ZoomableSwiftUI
+
+
+enum ImageType: String, CaseIterable {
+    case infinity
+    case random
+    
+    var size: CGSize? {
+        switch self {
+        case .infinity:
+            return nil
+        case .random:
+            return .init(
+                width: Int.random(in: 100...300),
+                height: Int.random(in: 100...300)
+            )
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var selectedImage: ImageType = .random
+    let randomImageUrl = "https://source.unsplash.com/random"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Picker(selection: $selectedImage, label: Text("image size")) {
+                ForEach(ImageType.allCases, id: \.rawValue) {
+                    Text("\($0.rawValue)")
+                        .tag($0)
+                }
+            }
+            .pickerStyle(.segmented)
+            .frame(height: 48.0)
+          
+            Spacer(minLength: 0.0)
+            
+            ZoomableView(url: randomImageUrl, frameSize: selectedImage.size)
+                .overlay {
+                    ZStack {
+                        Color.orange
+                        Text("Content area")
+                    }
+                    .opacity(0.15)
+                }
+
+            Spacer(minLength: 0)
+
+            HStack(alignment: .bottom) {
+                Spacer()
+                Text("Footer area")
+                Spacer()
+            }
+            .frame(height: 48.0)
+            .background(Color.gray)
+            .opacity(0.15)
         }
         .padding()
     }
