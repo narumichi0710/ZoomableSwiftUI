b94bf3f2990f7c0bfcd56c0a461a73d2fd186b03
quando <quando@quandonoMacBook-Pro.local>
2023-07-09 17:15:54 +0900

    init

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by quando on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
